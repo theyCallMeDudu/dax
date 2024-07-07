a8eccc298d62761e966290b605a7ea4e2a238f4b
Eduardo Coelho <coelho.edu96@gmail.com>
2024-07-07 16:35:33 -0300

    Started frontend

---
+++ dax-frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexModule } from './pages/index/index.module';
+import { MenuComponent } from './shared/menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IndexModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
